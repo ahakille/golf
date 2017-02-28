7a462eec4fdd548fbbbf56c38815879217b9bc09
Daniel von Koppy <vonkoppy@gmail.com>
2017-02-28 12:07:09 +0100

    Lagt till funktion för radering av bollar

--- Golf4/Golf4/Models/MemberModels.cs
+++ Golf4/Golf4/Models/MemberModels.cs
@@ -19,39 +19,6 @@ namespace Golf4.Models
         public string Telephone { get; set; } = "";
         public bool Payment { get; set; } = false;
 
-        public static void Reservation(ReservationModels reservation)
-        {
-            PostgresModels Database = new PostgresModels();
-            Database.SqlNonQuery("INSERT INTO reservation(timestart, timeend, closed, user) VALUES(@timestart, @timeend, @closed, @user)", PostgresModels.list = new List<NpgsqlParameter>()
-            {                
-                 new NpgsqlParameter("@timestart", reservation.Timestart),
-                 new NpgsqlParameter("@timeend", reservation.Timeend),
-                 new NpgsqlParameter("@closed", reservation.Closed),
-                 new NpgsqlParameter("@user", reservation.ID)
-            });           
-        }
-
-        public static void Reservation(List<int> memberid, int reservationid)
-        {
-            PostgresModels Database = new PostgresModels();
-            foreach (int userid in memberid)
-            {
-                Database.SqlNonQuery("INSERT INTO balls(userid, reservationid) VALUES(@userid, @reservationid)", PostgresModels.list = new List<NpgsqlParameter>()
-                {
-                     new NpgsqlParameter("@userid", userid),
-                     new NpgsqlParameter("@reservationid", reservationid)
-                });
-            }
-        }
-
-        public static void RemoveReservation(int reservationid)
-        {
-            PostgresModels Database = new PostgresModels();
-            Database.SqlNonQuery("DELETE FROM balls WHERE reservationid = @reservationid; DELETE FROM reservation WHERE id = @reservationid", PostgresModels.list = new List<NpgsqlParameter>()
-            {
-                new NpgsqlParameter("@reservationid", reservationid),
-            });
-        }
     }
 
 }
