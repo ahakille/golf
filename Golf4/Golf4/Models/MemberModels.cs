b909fb84597d724b10687230b53b6a43e05bfa43
isakssonleonard <leis1300@student.miun.se>
2017-03-02 14:41:03 +0100

    lagt till lista igen pga merge

--- Golf4/Golf4/Models/MemberModels.cs
+++ Golf4/Golf4/Models/MemberModels.cs
@@ -1,57 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace Golf4.Models
 {
     public class MemberModels
     {
-        public class MembersViewModel
-        {
-            [Display(Name = "Förnamn")]
-            public string Firstname { get; set; } = "";
-            [Display(Name = "Efternamn")]
-            public string Lastname { get; set; } = "";
-            [Display(Name = "Adress")]
-            public string Adress { get; set; } = "";
-            [Display(Name = "Postkod")]
-            public string Postalcode { get; set; } = "";
-            [Display(Name = "Ort")]
-            public string City { get; set; } = "";
-            [Display(Name = "Email")]
-            [EmailAddress]
-            public string Email { get; set; } = "";
-            [Display(Name = "")]
-            public int Gender { get; set; } 
-            [Display(Name = "HCP")]
-            public double HCP { get; set; }
-            
-        }
-      
-        
-            public int ID { get; set; } = 0;
-            [Display(Name = "Förnamn")]
-            public string Firstname { get; set; } = "";
-            [Display(Name = "Efternamn")]
-            public string Lastname { get; set; } = "";
-            [Display(Name = "Adress")]
-            public string Adress { get; set; } = "";
-            [Display(Name = "Postkod")]
-            public string Postalcode { get; set; } = "";
-            [Display(Name = "Ort")]
-            public string City { get; set; } = "";
-            [Display(Name = "Email")]
-            [EmailAddress]
-            public string Email { get; set; } = "";
-            [Display(Name = "Email")]
-            public int Gender { get; set; } = 0;
-            [Display(Name = "Email")]
-            public double HCP { get; set; } = 0.0;
-            [Display(Name = "Email")]
-            public int GolfID { get; set; } = 0;
-            [Display(Name = "Email")]
-            public int Membercategory { get; set; } = 0;
-            public string Telephone { get; set; } = "";
-            public bool Payment { get; set; } = false;
-        
+        public int ID { get; set; } = 0;
+        [Display(Name = "Förnamn")]
+        public string Firstname { get; set; } = "";
+        [Display(Name = "Efternamn")]
+        public string Lastname { get; set; } = "";
+        [Display(Name = "Adress")]
+        public string Adress { get; set; } = "";
+        [Display(Name = "Postkod")]
+        public string Postalcode { get; set; } = "";
+        [Display(Name = "Ort")]
+        public string City { get; set; } = "";
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+        public int Gender { get; set; } = 0;
+        public double HCP { get; set; } = 0.0;
+        public int GolfID { get; set; } = 0;
+        public int Membercategory { get; set; } = 0;
+        public string Telephone { get; set; } = "";
+        public bool Payment { get; set; } = false;
+
     }
 
 }
