e194a86cb14bb822e6a8201c3ec43b1c61f55224
Nicklas Persson <gorlingy@hotmail.com>
2017-03-03 01:54:25 +0100

    Sql members

--- Golf4/Golf4/Controllers/MemberController.cs
+++ Golf4/Golf4/Controllers/MemberController.cs
@@ -7,6 +7,7 @@ using Golf4.Models;
 using Microsoft.AspNet.Identity;
 using System.Security.Claims;
 using Npgsql;
+using System.Data;
 
 namespace Golf4.Controllers
 {
@@ -16,61 +17,66 @@ namespace Golf4.Controllers
         // GET: Member
         public ActionResult Index()
         {
+            MemberModels.MembersViewModel model = new MemberModels.MembersViewModel();
+            int test = 2;
             ClaimsIdentity i = new ClaimsIdentity();
             PostgresModels sql = new PostgresModels();
-            var dt = sql.SqlQuery("SELECT members.id, members.firstname, members.address,members.postalcode,members.city,members.email,members.telephone,members.hcp,members.golfid,membercategories.category,genders.gender  FROM members INNER JOIN membercategories ON members.membercategory = membercategories.id Inner JOIN genders ON members.gender = genders.id where members.id = '1'", PostgresModels.list = new List<NpgsqlParameter>()
+            DataTable dt = sql.SqlQuery("SELECT members.id, members.firstname,members.lastname, members.address,members.postalcode,members.city,members.email,members.telephone,members.hcp,members.golfid,membercategories.category,genders.gender  FROM members INNER JOIN membercategories ON members.membercategory = membercategories.id Inner JOIN genders ON members.gender = genders.id where members.id = @par1", PostgresModels.list = new List<NpgsqlParameter>()
             {
-                new NpgsqlParameter("@par1", "1")
-
+                new NpgsqlParameter("@par1", test)
             });
-            string test = i.Name; 
-
-            return View();
-        }
-
-        // GET: Member/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Member/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Member/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection, Models.MemberModels.MembersViewModel model)
-        {
-          int test=  model.ID;
-            try
+            foreach (DataRow dr in dt.Rows)
             {
-                // TODO: Add insert logic here
+                model.ID =(int)dr["id"];
+                model.Firstname = (string)dr["firstname"];
+                model.Lastname = (string)dr["lastname"];
+                model.Adress = (string)dr["address"];
+                model.Postalcode = (string)dr["postalcode"];
+                model.City = (string)dr["city"];
+                model.Email = (string)dr["email"];
+                model.Telephone = (string)dr["telephone"];
+                model.GolfID = (string)dr["golfid"];
+                model.HCP = (Double)dr["hcp"];
+                model.Membercategory = (string)dr["category"];
+                model.Gender = (string)dr["gender"];
+            }
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            TempData["user"] = model;
+
+            return View(model);
         }
+
 
         // GET: Member/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public ActionResult Edit()
         {
-            return View();
+          
+            return View(TempData["user"]);
         }
 
         // POST: Member/Edit/5
         [HttpPost]
         public ActionResult Edit(FormCollection collection)
         {
-            
+
+            string fname = collection["Firstname"],lname = collection[3];
             try
             {
                 
+                PostgresModels sql = new PostgresModels();
+                sql.SqlNonQuery("UPDATE members SET firstname=@par2, address=@par3,postalcode=@par4,city=@par5,email=@par6,telephone=@par7,hcp=@par8 WHERE id = '2'", PostgresModels.list = new List<NpgsqlParameter>()
+            {
+                new NpgsqlParameter("@par2", fname),
+                new NpgsqlParameter("@par3", lname),
+                new NpgsqlParameter("@par4", collection[4]),
+                new NpgsqlParameter("@par5", collection[5]),
+                new NpgsqlParameter("@par6", collection[6]),
+                new NpgsqlParameter("@par7", collection[7]),
+                new NpgsqlParameter("@par8", collection[8]),
+                new NpgsqlParameter("@par9", collection[9]),
+                new NpgsqlParameter("@par1", collection[9])
+            });
 
                 return RedirectToAction("Index");
             }
--- Golf4/Golf4/Models/MemberModels.cs
+++ Golf4/Golf4/Models/MemberModels.cs
@@ -7,30 +7,30 @@ namespace Golf4.Models
     {
         public class MembersViewModel
         {
-            public int ID { get; set; } = 1;
+            public int ID { get; set; } 
             [Display(Name = "Förnamn")]
-            public string Firstname { get; set; } = "";
+            public string Firstname { get; set; } 
             [Display(Name = "Efternamn")]
-            public string Lastname { get; set; } = "";
+            public string Lastname { get; set; } 
             [Display(Name = "Adress")]
-            public string Adress { get; set; } = "";
+            public string Adress { get; set; } 
             [Display(Name = "Postkod")]
-            public string Postalcode { get; set; } = "";
+            public string Postalcode { get; set; } 
             [Display(Name = "Ort")]
-            public string City { get; set; } = "";
+            public string City { get; set; } 
             [Display(Name = "Email")]
             [EmailAddress]
-            public string Email { get; set; } = "";
+            public string Email { get; set; } 
             [Display(Name = "Kön")]
             public string Gender { get; set; } 
             [Display(Name = "HCP")]
             public double HCP { get; set; }
             [Display(Name = "Golfid")]
-            public int GolfID { get; set; } = 0;
+            public string GolfID { get; set; } 
             [Display(Name = "Medlemskategori")]
-            public int Membercategory { get; set; } = 0;
+            public string Membercategory { get; set; } 
             [Display(Name = "Telefonnummer")]
-            public string Telephone { get; set; } = "";
+            public string Telephone { get; set; } 
             [Display(Name = "Betalat medlemsavgift")]
             public bool Payment { get; set; } = false;
         }
