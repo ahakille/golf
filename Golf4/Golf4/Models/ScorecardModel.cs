06800a8823a1155c087d538e695d6a97015276d4
Nicklas Persson <gorlingy@hotmail.com>
2017-03-09 15:33:59 +0100

    Prop i scorecard

--- Golf4/Golf4/Models/ScorecardModel.cs
+++ Golf4/Golf4/Models/ScorecardModel.cs
@@ -7,5 +7,23 @@ namespace Golf4.Models
 {
     public class ScorecardModel
     {
+        public int Hole { get; set; }
+        public int Yellow { get; set; }
+        public int Red { get; set; }
+
+        public int Par { get; set; }
+        public int HCP { get; set; }
+        public int User_id1 { get; set; }
+        public int User_1_Hcp { get; set; }
+        public int User_id2 { get; set; }
+        public int User_2_Hcp { get; set; }
+        public int User_id3 { get; set; }
+        public int User_3_Hcp { get; set; }
+        public int User_id4 { get; set; }
+        public int User_4_Hcp { get; set; }
+    }
+    public class score
+    {
+
     }
 }
