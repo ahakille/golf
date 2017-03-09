a2c7b7ab5c8ad2e474a60fed9ed953f379c76975
Nicklas Persson <gorlingy@hotmail.com>
2017-03-09 12:41:21 +0100

    Fixat avbokningsmetod

--- Golf4/Golf4/Controllers/ReservationController.cs
+++ Golf4/Golf4/Controllers/ReservationController.cs
@@ -293,10 +293,16 @@ namespace Golf4.Controllers
             model.ID = Convert.ToInt16(Request.QueryString["member"]);
             try
             {
-                //if(makebooking.CheckReservationUser(model.))
-                //{
 
-                //}
+                Tuple<bool,int> ids =  makebooking.CheckReservationUser(model.Timestart, model.ID);
+                if (ids.Item1)
+                {
+                    makebooking.DeleteReservation(ids.Item2);
+                }
+                else
+                {
+                    makebooking.DeleteBoll(ids.Item2, model.ID);
+                }
                
                 
                 return RedirectToAction("admin", "reservation", new { validdate = model.Timestart });
