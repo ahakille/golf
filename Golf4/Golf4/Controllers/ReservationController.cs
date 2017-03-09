c03dfed48b5b86204714da5b925f66e8a279878b
Daniel von Koppy <vonkoppy@gmail.com>
2017-03-09 15:32:05 +0100

    Nu blir det grått i de tidsspann som är stängda! :D Går inte heller att boka när det är stängt!

--- Golf4/Golf4/Controllers/ReservationController.cs
+++ Golf4/Golf4/Controllers/ReservationController.cs
@@ -24,32 +24,29 @@ namespace Golf4.Controllers
             DataTable RBD = new DataTable();
             try
             {
+                PostgresModels Database = new PostgresModels();
                 {
-                    PostgresModels Database = new PostgresModels();
                     {
-                        {
-                            RBD = Database.SqlQuery("SELECT reservations.id as \"rid\", reservations.timestart as \"rts\", reservations.timeend as \"rte\", reservations.closed as \"rc\", reservations.user_id as \"ru\", balls.userid as \"bu\", balls.reservationid as \"bi\", members.id as \"mid\", members.firstname as \"mf\", members.lastname as \"ml\", members.address as \"ma\", members.postalcode as \"mp\", members.city as \"mc\", members.email as \"me\", members.telephone as \"mt\", members.hcp as \"mh\", members.golfid as \"mgi\", members.gender as \"mg\", members.membercategory as \"mct\", members.payment \"mpa\" FROM reservations JOIN balls ON balls.reservationid = reservations.id JOIN members ON balls.userid = members.id WHERE date(timestart) = current_date ORDER BY timestart", PostgresModels.list = new List<NpgsqlParameter>());
-                        }
+                        RBD = Database.SqlQuery("SELECT reservations.id as \"rid\", reservations.timestart as \"rts\", reservations.timeend as \"rte\", reservations.closed as \"rc\", reservations.user_id as \"ru\", balls.userid as \"bu\", balls.reservationid as \"bi\", members.id as \"mid\", members.firstname as \"mf\", members.lastname as \"ml\", members.address as \"ma\", members.postalcode as \"mp\", members.city as \"mc\", members.email as \"me\", members.telephone as \"mt\", members.hcp as \"mh\", members.golfid as \"mgi\", members.gender as \"mg\", members.membercategory as \"mct\", members.payment \"mpa\" FROM reservations JOIN balls ON balls.reservationid = reservations.id JOIN members ON balls.userid = members.id WHERE date(timestart) = current_date ORDER BY timestart", PostgresModels.list = new List<NpgsqlParameter>());
                     }
-                    List<ReservationModels> reservationlist = new List<ReservationModels>();
-                    foreach (DataRow dr in RBD.Rows)
-                    {
-                        ReservationModels Reservation2 = new ReservationModels();
-                        Reservation2.MemberID = (int)dr["mid"];
-                        Reservation2.MemberHCP = (double)dr["mh"];
-                        Reservation2.MemberGolfID = dr["mgi"].ToString();
-                        Reservation2.MemberGender = (int)dr["mg"];
-                        Reservation2.ID = (int)dr["rid"];
-                        Reservation2.Timestart = Convert.ToDateTime(dr["rts"]);
-                        Reservation2.Timeend = Convert.ToDateTime(dr["rte"]);
-                        Reservation2.Closed = (bool)dr["rc"];
-                        Reservation2.User = (int)dr["ru"];
-                        reservationlist.Add(Reservation2);
-                    }
-                    
-                    ViewBag.List = reservationlist;
-                    
                 }
+                List<ReservationModels> reservationlist = new List<ReservationModels>();
+                foreach (DataRow dr in RBD.Rows)
+                {
+                    ReservationModels Reservation2 = new ReservationModels();
+                    Reservation2.MemberID = (int)dr["mid"];
+                    Reservation2.MemberHCP = (double)dr["mh"];
+                    Reservation2.MemberGolfID = dr["mgi"].ToString();
+                    Reservation2.MemberGender = (int)dr["mg"];
+                    Reservation2.ID = (int)dr["rid"];
+                    Reservation2.Timestart = Convert.ToDateTime(dr["rts"]);
+                    Reservation2.Timeend = Convert.ToDateTime(dr["rte"]);
+                    Reservation2.Closed = (bool)dr["rc"];
+                    Reservation2.User = (int)dr["ru"];
+                    reservationlist.Add(Reservation2);
+                }
+
+                ViewBag.List = reservationlist;
                 Reservation.datepicker = DateTime.Now.Date.ToShortDateString();
                 return View(Reservation);
             }
