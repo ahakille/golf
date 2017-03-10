b5fcd52f42a0803808a55411bdcd2ca865ebb8de
isakssonleonard <leis1300@student.miun.se>
2017-03-10 12:04:33 +0100

    jobbat mer med att skica mail

--- Golf4/Golf4/Models/EmailModels.cs
+++ Golf4/Golf4/Models/EmailModels.cs
@@ -2,31 +2,52 @@
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Golf4.Models
 {
     public abstract class EmailModels
     {
-        public static void skickaEmail(string Sender, string Password, string To, string Subject, string Message)
+        public static void skickaEmail(string Sender, string Password, List<string> To, string Subject, string Message)
         {
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-            client.EnableSsl = true;
-            client.Timeout = 10000;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential(Sender,Password);
-
-            MailMessage mail = new MailMessage(Sender,To,Subject,Message);
-            mail.BodyEncoding = Encoding.UTF8;
-            mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
-
-            try
+            foreach (string to in To)
             {
-                client.Send(mail);
-            }
-            catch (Exception ex)
-            {
-                string error = ex.Message;
+                string Emailsmtp = "";
+
+                if (to.Contains(".gmail.com"))
+                {
+                    Emailsmtp = "smtp.gmail.com";
+                }
+
+                else if (to.Contains(".yahoo.com"))
+                {
+                    Emailsmtp = "smtp.mail.yahoo.com";
+                }
+
+                else
+                {
+                    Emailsmtp = "smtp.live.com";
+                }
+
+                SmtpClient client = new SmtpClient(Emailsmtp, 587);
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(Sender,Password);
+
+                MailMessage mail = new MailMessage(Sender, to, Subject, Message);
+                mail.BodyEncoding = Encoding.UTF8;
+                mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+                try
+                {
+                    client.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                    string error = ex.Message;
+                }
             }
         }
     }
