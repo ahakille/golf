250ca5552cca6ee70824bb886d1452fc780ae156
Nicklas Persson <gorlingy@hotmail.com>
2017-03-14 16:25:02 +0100

    Fixat modelen

--- Golf4/Golf4/Models/CompetitionModels.cs
+++ Golf4/Golf4/Models/CompetitionModels.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,14 +9,25 @@ namespace Golf4.Models
 {
     public class CompetitionModels
     {
-        public int Competition_id { get; set; }
+        //public int Competition_id { get; set; }
+        [Required]
+        [Display(Name="Tävlingsnamnet")]
         public string Name { get; set; }
-        public DateTime CloseTime { get; set; }
-        public int MaxPlayers { get; set; }
-        public bool Publish { get; set; }
-        public int Reservation_id { get; set; }
+        [Required]
+        [Display(Name = "Startdatum och tid")]
         public DateTime Timestart { get; set; }
+        [Required]
+        [Display(Name = "Slutdatum och ti")]
         public DateTime Timeend { get; set; }
+        [Required]
+        [Display(Name = "Sista anmälningsdag")]
+        public DateTime CloseTime { get; set; }
+        [Required]
+        [Display(Name = "Max antal spelare")]
+        public int MaxPlayers { get; set; }
+        //public bool Publish { get; set; }
+        //public int Reservation_id { get; set; }
+
 
 
 
