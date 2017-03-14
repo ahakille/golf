8ced8e3ced83ac12357b9b35986e58e397d77351
Nicklas Persson <gorlingy@hotmail.com>
2017-03-14 16:03:23 +0100

    Prop för tävling

--- Golf4/Golf4/Models/CompetitionModel.cs
+++ Golf4/Golf4/Models/CompetitionModel.cs
@@ -7,5 +7,21 @@ namespace Golf4.Models
 {
     public class CompetitionModel
     {
+        public int Competition_id { get; set; }
+        public string Name { get; set; }
+        public DateTime CloseTime { get; set; }
+        public int MaxPlayers { get; set; }
+        public bool Publish { get; set; }
+        public int Reservation_id { get; set; }
+        public DateTime Timestart { get; set; }
+        public DateTime Timeend { get; set; }
+
+
+
+        public class MakeCompetition
+        {
+
+        }
     }
+
 }
