de74f25001119a190020d7e4aab6d54bd32b1bb9
malin <malin@DESKTOP-4TABKED>
2017-03-15 11:01:18 +0100

    Grejat med att hämta alla tävlingar.

--- Golf4/Golf4/Controllers/CompetitionController.cs
+++ Golf4/Golf4/Controllers/CompetitionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 using Golf4.Models;
 
 namespace Golf4.Controllers
@@ -12,6 +13,7 @@ namespace Golf4.Controllers
         // GET: Competition
         public ActionResult Index()
         {
+            //DataTable dt = Models.CompetitionModels.Get
             return View();
         }
         public ActionResult create()
