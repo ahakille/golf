5ec790416ab340f028ec4f64e34a31452c1b93f4
Nicklas Persson <gorlingy@hotmail.com>
2017-03-15 09:25:31 +0100

    arbetar med metod för att skapa täving

--- Golf4/Golf4/Models/CompetitionModels.cs
+++ Golf4/Golf4/Models/CompetitionModels.cs
@@ -4,12 +4,13 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Npgsql;
 
 namespace Golf4.Models
 {
     public class CompetitionModels
     {
-        //public int Competition_id { get; set; }
+        public int Competition_id { get; set; }
         [Required]
         [Display(Name="Tävlingsnamnet")]
         public string Name { get; set; }
@@ -25,15 +26,22 @@ namespace Golf4.Models
         [Required]
         [Display(Name = "Max antal spelare")]
         public int MaxPlayers { get; set; }
-        //public bool Publish { get; set; }
-        //public int Reservation_id { get; set; }
+        public bool Publish { get; set; }
+        public int Reservation_id { get; set; }
 
 
 
 
         public class MakeCompetition
         {
+            public void CreateCompetition(string name, DateTime start, DateTime end ,DateTime close, int maxplayer)
+            {
+                ReservationModels.MakeBooking makebooking = new ReservationModels.MakeBooking();
 
+                makebooking.MakeReservations(start,end,false,1);
+                PostgresModels sql = new PostgresModels();
+                
+            }
         }
     }
 
