c2b635b99f7105114469f14ca93eb53bcf25944b
malin <malin@DESKTOP-4TABKED>
2017-03-15 14:28:05 +0100

    Fipplar med listan för tävlingar och för att kunna boka

--- Golf4/Golf4/Controllers/ContestController.cs
+++ Golf4/Golf4/Controllers/ContestController.cs
@@ -16,7 +16,7 @@ namespace Golf4.Controllers
         {
             PostgresModels Database = new PostgresModels();
             DataTable dt = new DataTable("data");
-            dt = Database.SqlQuery("SELECT contests.name AS \"Namn\", contests.description AS \"Beskrivning\", reservations.timestart AS \"Start\", reservations.timeend AS \"Slut\",  contests.closetime AS \"Sista anm.\" FROM reservations, contests WHERE reservations.id = contests.reservationid AND reservations.timestart > CURRENT_DATE AND contests.closetime > CURRENT_DATE", PostgresModels.list = new List<NpgsqlParameter>()
+            dt = Database.SqlQuery("SELECT contests.name AS \"Namn\", contests.description AS \"Beskrivning\", reservations.timestart AS \"Start\", reservations.timeend AS \"Slut\",  contests.closetime AS \"Sista anm.\", contests.id FROM reservations, contests WHERE reservations.id = contests.reservationid AND reservations.timestart > CURRENT_DATE AND contests.closetime > CURRENT_DATE", PostgresModels.list = new List<NpgsqlParameter>()
             {
                 //new NpgsqlParameter("@time", DateTime.Now)
             });
