9ee643422a62b229f146531919a8ab2edc5c4910
malin <malin@DESKTOP-4TABKED>
2017-03-16 13:04:30 +0100

    Ändrat i sql-fråga förmedlemmarna i en tävling.

--- Golf4/Golf4/Models/ContestModels.cs
+++ Golf4/Golf4/Models/ContestModels.cs
@@ -88,7 +88,7 @@ namespace Golf4.Models
             {
                 PostgresModels Database = new PostgresModels();
                 DataTable dt = new DataTable("data");
-                dt = Database.SqlQuery("SELECT golfid, firstname, lastname, hcp, gender, membercategory FROM members LEFT JOIN players ON members.id = players.memberid LEFT JOIN contests ON players.contestid = contests.id WHERE contests.id = @contestid", PostgresModels.list = new List<NpgsqlParameter>()
+                dt = Database.SqlQuery("SELECT golfid AS \"GolfID\", firstname AS \"Förnamn\", lastname AS \"Efternamn\", hcp AS \"HCP\", gender AS \"Kön\", membercategory AS \"Medlemskategori\" FROM members LEFT JOIN players ON members.id = players.memberid LEFT JOIN contests ON players.contestid = contests.id WHERE contests.id = @contestid", PostgresModels.list = new List<NpgsqlParameter>()
                 {
                     new NpgsqlParameter("@contestid", contestid)
                 });
